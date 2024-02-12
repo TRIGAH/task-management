9f6a8f2dfef761a90eb13ca7b9d45687ba04ac38
Trigah <ijenrandy2@gmail.com>
2024-02-12 16:05:18 +0100

    Tested new approach with session

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,session
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime,timedelta
 
 app = Flask(__name__)
+app.secret_key='hdfdfkjgfjgfjlfgdj'
+app.config['SESSION_COOKIE_NAME'] = 'custom_session_cookie'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+# db.create_all()
+
+
+##Cafe TABLE Configuration
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+
 
 
 @app.route('/',methods=['POST','GET'])
 def home():
-    tasks=[]
-    task = request.form.get('task')
-    tasks.append(task)
-    return render_template('tasks.html',tasks=tasks)
+    #Create and empty list
+    session['data']=[]
+
+    # Get task name from Form
+    if request.method == 'POST':
+        task = request.form.get('task')
+
+        # Add task to the List
+        session['data'].append(task)
+
+    return render_template('tasks.html',tasks=session['data'])
 
 
 if __name__=='__main__':
